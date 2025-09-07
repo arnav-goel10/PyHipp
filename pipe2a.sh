0434fce6804f55c5b069e67134c7fdc3baa178c6
AWS ParallelCluster user <ec2-user@ip-10-0-11-15.ap-southeast-1.compute.internal>
2025-09-07 16:52:06 +0000

    Update pipe2a

--- pipe2a.sh
+++ pipe2a.sh
@@ -3,12 +3,11 @@
 # first job - submit rplparallel (splitting)
 jid1=$(sbatch /data/src/PyHipp/rplparallel-slurm.sh)
 
-# second job - submit rse only after rplparallel finishes successfully
-jid2=$(sbatch --dependency=afterok:${jid1##* } /data/src/PyHipp/rse-slurm.sh)
+# second job - no dependencies, called from the day directory
+jid2=$(sbatch /data/src/PyHipp/rse-slurm.sh)
 
-# third set of jobs - depend on rse
+# third set of jobs - depends on rse-slurm.sh, called from the day directory
 jid3=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs1a-slurm.sh)
 jid4=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs2a-slurm.sh)
 jid5=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs3a-slurm.sh)
 jid6=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs4a-slurm.sh)
-
