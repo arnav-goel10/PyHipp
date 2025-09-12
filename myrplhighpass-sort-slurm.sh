ec61184153dc5bc655ed2e1c77751261e0f98e74
AWS ParallelCluster user <ec2-user@ip-10-0-13-217.ap-southeast-1.compute.internal>
2025-09-12 17:49:44 +0000

    Add my* scripts to avoid conflicts with upstream versions

---
+++ myrplhighpass-sort-slurm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=04:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH -J "rplhighpass_sort"
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o rplhighpass-sort-slurm.%N.%j.out
+#SBATCH -e rplhighpass-sort-slurm.%N.%j.err
+
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
+python -u -c "import PyHipp as pyh; \
+import time; \
+pyh.RPLHighPass(saveLevel=1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
+print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
